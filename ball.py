ffc006361918b69762e7a680a1aef1a4da4adb94
Khaled Elsayed <khaled.elsayed2206@gmail.com>
2025-10-17 21:18:38 +0300

    Add the Ball Bouncing Logic and updating the main file

--- ball.py
+++ ball.py
@@ -7,12 +7,16 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
         
         
     def move_up_right(self):
-        new_x = self.xcor() + 13
-        new_y = self.ycor() + 10
+        new_x = self.xcor() + self.x_move   #13
+        new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-        
+
+    def bounce(self):
+        self.y_move *= -1    
         
--- main.py
+++ main.py
@@ -25,7 +25,9 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move_up_right()
-    
+    if ball.ycor() >= 280 or ball.ycor() <= -280:
+        ball.bounce()
+        
 
 
 
